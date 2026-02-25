395857c5e435d899065997a86b902820ff606cca
Your Name <ansokwin@gmail.com>
2026-02-25 15:09:44 +0700

    First commit

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Example of another protected route
+    Route::get('/dashboard', function () {
+        return response()->json([
+            'message' => 'Welcome to the dashboard. You are authenticated!',
+        ]);
+    });
+});
